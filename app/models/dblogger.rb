c75a5e70917b99c177086a6b383c077c3381dcf9
Victor Brylew <victor@virlin.MOUSEHOME>
2009-04-02 11:15:36 +0300

    Making logger stable with unusual types of log events.

--- app/models/dblogger.rb
+++ app/models/dblogger.rb
@@ -1,7 +1,5 @@
 module ActiveSupport
   class BufferedLogger
-    @@PREDEFINED_EVENT_TYPES = %w(rendering parameters session attempting rendered cookie failed redirected filter migrating processing completed)
-
     def add(severity, message = nil, progname = nil, &block)
       return if @level > severity
       message = (message || (block && block.call) || progname).to_s
@@ -18,7 +16,7 @@ module ActiveSupport
         :request_id => $$
       }
 
-      data_specific_for_this_type_of_event = @@PREDEFINED_EVENT_TYPES.include?(content_type) ? send('process_'+content_type+'_type_of_event', message) : {}
+      data_specific_for_this_type_of_event = send('process_'+content_type+'_type_of_event', message)
 
       LoggedEvent.create! common_item_data.merge(data_specific_for_this_type_of_event)
     end
@@ -59,12 +57,10 @@ module ActiveSupport
       }
     end
 
-    (@@PREDEFINED_EVENT_TYPES - ['processing', 'completed']).each do |name|
-      class_eval <<-EOT, __FILE__, __LINE__
-        def process_#{name}_type_of_event message
-          {}
-        end
-      EOT
+    def method_missing(method_name, *args)
+      if method_name.to_s =~ /process_(.+)_type_of_event$/
+        return {}
+      end
     end
   end
 end
